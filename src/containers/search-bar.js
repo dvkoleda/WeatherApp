3bca56958bae51dfd51349300a36e160ee6a9542
koleda_d <deniskoleda@iba.by>
2017-05-29 16:41:04 +0300

    Added template for the search-bar component.

---
+++ src/containers/search-bar.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Koleda_D on 29.05.2017.
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <form className="input-group">
+                <input />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">
+                        Submit
+                    </button>
+                </span>
+            </form>
+        )
+    }
+}
+
+export default SearchBar;
