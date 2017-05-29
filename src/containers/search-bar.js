37999ba0de27c3c4b9f55d59ee9ec00a9dda17d5
koleda_d <deniskoleda@iba.by>
2017-05-29 17:25:23 +0300

    added state to a search-bar component

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -5,10 +5,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SearchBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({term: event.target.value});
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input
+                    placeholder="Get a five-day forecast in you favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">
                         Submit
